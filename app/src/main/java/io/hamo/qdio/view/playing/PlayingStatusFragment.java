1a25a1a4ed7d9c2daeba4cbc65b19d32f5c04103
hcliffordson <cliffords.contact@gmail.com>
2018-09-30 19:13:06 +0200

    Added logic for playingstatusfragment. Fetching playing-info in thread

--- app/src/main/java/io/hamo/qdio/view/playing/PlayingStatusFragment.java
+++ app/src/main/java/io/hamo/qdio/view/playing/PlayingStatusFragment.java
@@ -1,5 +1,6 @@
 package io.hamo.qdio.view.playing;
 
+import android.arch.lifecycle.Observer;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,10 +9,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import io.hamo.qdio.R;
+import io.hamo.qdio.music.Track;
 
 public class PlayingStatusFragment extends Fragment {
+
+    private PlayingStatusViewModel viewModel;
+    private TextView trackName;
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -20,12 +27,23 @@ public class PlayingStatusFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new PlayingStatusViewModel();
+
+        viewModel.getCurrentlyPlayingTrack().observe(this, new Observer<Track>() {
+            @Override
+            public void onChanged(@Nullable Track track) {
+                trackName.setText(track.getName());
+            }
+        });
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_playing_status, container, false);
+        trackName = (TextView)view.findViewById(R.id.trackName);
         return view;
+
     }
 }
--- app/src/main/java/io/hamo/qdio/view/playing/PlayingStatusViewModel.java
+++ app/src/main/java/io/hamo/qdio/view/playing/PlayingStatusViewModel.java
@@ -3,25 +3,47 @@ package io.hamo.qdio.view.playing;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.os.Handler;
+import android.util.Log;
+
 
 import io.hamo.qdio.music.Track;
+import io.hamo.qdio.playback.PlayerFactory;
 
 public class PlayingStatusViewModel extends ViewModel {
     private static final int STATUS_POLL_INTERVAL = 500;
     private final MutableLiveData<Track> currentlyPlayingTrack;
     private final MutableLiveData<Long> currentPosition;
+    private final MutableLiveData<String> currentAlbumImageUrl;
     //private final MutableLiveData<PlayerState> currentPlayerState;
+    private Handler handler = new Handler();
 
     public PlayingStatusViewModel() {
         currentlyPlayingTrack = new MutableLiveData<>();
         currentPosition = new MutableLiveData<>();
+        currentAlbumImageUrl = new MutableLiveData<>();
+        //TODO change this
+        handler.postDelayed(runnableCode, 2000);
         //currentPlayerState = new MutableLiveData<>();
 
 
     }
 
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            // Do something here on the main thread
+            currentlyPlayingTrack.postValue(PlayerFactory.getPlayer().getCurrentTrack());
+            currentAlbumImageUrl.postValue(PlayerFactory.getPlayer().getCurrentTrack().getImageURL());
+            Log.d("Handlers", "Called on main thread");
+            // Repeat this the same runnable code block again another .5 seconds
+            // 'this' is referencing the Runnable object
+            handler.postDelayed(this, STATUS_POLL_INTERVAL);
+        }
+    };
 
 
+    //public LiveData<String> //Bitmap
 
     public LiveData<Track> getCurrentlyPlayingTrack() {
         return currentlyPlayingTrack;
