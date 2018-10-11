e8e26eac2beed6721e4fa38df59f4b33081a2f66
oggewallgren <oskarwallgren@icloud.com>
2018-10-11 08:35:26 +0200

    added webView to welcomeActivity for animating progress bar

--- app/src/main/java/io/hamo/qdio/view/WelcomeActivity.java
+++ app/src/main/java/io/hamo/qdio/view/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -12,6 +13,8 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
@@ -25,6 +28,7 @@ import io.hamo.qdio.room.init.RoomCreationService;
 public class WelcomeActivity extends FragmentActivity {
     private ProgressBar roomConnectProgress;
     private LinearLayout buttonGroup;
+    private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,13 @@ public class WelcomeActivity extends FragmentActivity {
         checkPermissions();
         roomConnectProgress = findViewById(R.id.roomConnectProgress);
         buttonGroup = findViewById(R.id.buttonGroup);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("");
     }
 
     private void checkPermissions() {
@@ -75,6 +86,22 @@ public class WelcomeActivity extends FragmentActivity {
     public void joinRoom(View view) {
         Intent intent = new Intent(this, RoomDiscoveryActivity.class);
         startActivity(intent);
+    }
+    //class to use for animated progress bar, like a GIF
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView webView, String url, Bitmap gif) {
+            webView.setVisibility(webView.INVISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView webView, String url) {
+            roomConnectProgress.setVisibility(View.GONE);
+
+            webView.setVisibility(View.VISIBLE);
+            super.onPageFinished(webView, url);
+
+        }
     }
 
 
